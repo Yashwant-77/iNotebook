0870b5a94a405fa0022a82ae5d4861ed1158df07
Yashwant Chouhan <105690609+Yashwant-77@users.noreply.github.com>
2025-01-25 18:17:01 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -6,7 +6,7 @@ function Login({ showAlert }) {
   const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("https://inotebook-2-748t.onrender.com/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
